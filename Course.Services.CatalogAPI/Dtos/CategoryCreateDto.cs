1cc738bfdbc1d89ffbffd46edc2bef58c9ec7839
NURSULTAN SULTANOV <nursultan.sultanov.s@student.aztu.edu.az>
2022-06-09 19:53:04 +0400

    'feat : added Dto classes'

---
+++ Course.Services.CatalogAPI/Dtos/CategoryCreateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Course.Services.CatalogAPI.Dtos
+{
+    public class CategoryCreateDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Course.Services.CatalogAPI/Dtos/CategoryUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Course.Services.CatalogAPI.Dtos
+{
+    public class CategoryUpdateDto
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
