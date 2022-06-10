95558e85ce6f24d6ea650ff89a7746c8075fe416
NURSULTAN SULTANOV <nursultan.sultanov.s@student.aztu.edu.az>
2022-06-10 13:27:44 +0400

    feat: added connection settings

---
+++ Course.Services.CatalogAPI/ConnectionSettings/IDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace CourseApp.Services.CatalogAPI.ConnectionSettings
+{
+    public interface IDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+
+        public string CourseCollectionName { get; set; }
+
+        public string CategoryCollectionName { get; set; }
+
+        public string DatabaseName { get; set; }
+    }
+}
